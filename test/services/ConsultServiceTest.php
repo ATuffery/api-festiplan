0118820fbcde78673f755d0e3b881d3493405217
d-clem <85625458+cleeem@users.noreply.github.com>
2024-03-18 11:03:55 +0100

    ajout test
    Co-Authored-By: LolineF <120092727+LolineF@users.noreply.github.com>

--- test/services/ConsultServiceTest.php
+++ test/services/ConsultServiceTest.php
@@ -36,7 +36,7 @@ class ConsultServiceTest extends TestCase
             $this->pdo->beginTransaction();
             // Given une base de données connectée avec plusieurs festivals
             // When on affiche tout les festival
-            $result = $this->consultService::consultListFestival($this->pdo);
+            $result = $this->consultService::consultListFestival($this->pdo, "a42798bf979c69734c8f83c708610bca824dfc4e44f5c84cca72999bddf0f5725ba276");
             // Then les festivals s'affiche dans l'ordre de représentation
             // (les festivals à venir les plus proches en premier, suivis de ceux qui auront lieu plus tard)
             $premiereLigne = $result[0];
@@ -64,8 +64,8 @@ class ConsultServiceTest extends TestCase
             $sql = "DELETE FROM festival WHERE idFestival > 10";
             $this->pdo->query($sql);
             // When on affiche tout les festival
-            $result = $this->consultService::consultListFestival($this->pdo);
-            // Then
+            $result = $this->consultService::consultListFestival($this->pdo, "a42798bf979c69734c8f83c708610bca824dfc4e44f5c84cca72999bddf0f5725ba276");
+            // Then aucun festival ne s'afiche
             $this->assertEquals(0,count($result));
             $this->pdo->rollBack();
         } catch (\PDOException $e) {
@@ -75,14 +75,86 @@ class ConsultServiceTest extends TestCase
     }
 
     public function testConsultListFavoriteFestivalFullTable(){
+        try {
+            $this->pdo->beginTransaction();
+            // Given une base de données connectée avec plusieurs festivals et un utilisateur avec 2 festival en favori
+            $sql = "INSERT INTO favori (idUtilisateur, idFestival) VALUES (4, 11), (4, 12)";
 
+            $this->pdo->query($sql);
+            // When on affiche tout les festival
+            $result = $this->consultService::consultListFavoriteFestival($this->pdo, 4);
+            // Then les 2 festivals s'affiche dans l'ordre de représentation
+            // (les festivals à venir les plus proches en premier, suivis de ceux qui auront lieu plus tard)
+            $this->assertEquals(2,count($result));
+            $premiereLigne = $result[0];
+            $dateDebut = $premiereLigne["dateDebut"];
+            $isOrdered = true;
+            foreach ($result as $festival){
+                if(strtotime($dateDebut) <= strtotime($festival["dateDebut"])){
+                    $dateDebut = $festival["dateDebut"];
+                } else {
+                    $isOrdered = false;
+                }
+            }
+            $this->assertTrue($isOrdered);
+            $this->pdo->rollBack();
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack();
+            $this->fail("La base de données n'est pas accessible");
+        }
     }
 
     public function testConsultListFavoriteFestivalEmptyTableFavori(){
-
+        try {
+            $this->pdo->beginTransaction();
+            // Given une base de données connectée avec aucun festival
+            $sql = "DELETE FROM favori WHERE favori.idUtilisateur = 4";
+            $this->pdo->query($sql);
+            // When on affiche tout les festival
+            $result = $this->consultService::consultListFavoriteFestival($this->pdo, 4);
+            // Then aucun festival ne s'afiche
+            $this->assertEquals(0,count($result));
+            $this->pdo->rollBack();
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack();
+            $this->fail("La base de données n'est pas accessible", $e);
+        }
     }
 
-    public function testConsultListFavoriteFestivalEmptyTableFavori(){
+    public function testConsultListFavoriteFestivalEmptyTableFestival(){
+        try {
+            $this->pdo->beginTransaction();
+            // Given une base de données connectée avec aucun festival
+            $sql = "DELETE FROM festival WHERE idFestival > 10";
+            $this->pdo->query($sql);
+            // When on affiche tout les festival
+            $result = $this->consultService::consultListFavoriteFestival($this->pdo, 4);
+            // Then aucun festival ne s'afiche
+            $this->assertEquals(0,count($result));
+            $this->pdo->rollBack();
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack();
+            $this->fail("La base de données n'est pas accessible", $e);
+        }
+    }
+
+    public function testDetailsFestivalFullTable(){
+        try {
+            $this->pdo->beginTransaction();
+            // Given une base de données connectée avec aucun festival
+            $sql = "INSERT INTO festival VALUES (1,1,'Festival test','description','2024-03-18'202024-03-19','aaaa');
+                    INSERT INTO utilisateur VALUES (3,'test','test','test@test','test','test','ecef9bc7c00bac56615da543155fed2f20ad2eabfcf6882e4eda85ffedb76921216485');
+                    INSERT INTO equipeorganisatrice ";
+            $this->pdo->query($sql);
+            // When on affiche tout les festival
+            $result = $this->consultService::detailsFestival($this->pdo, 1);
+            // Then aucun festival ne s'afiche
+            $this->assertEquals(0,count($result));
+            $this->pdo->rollBack();
+        } catch (\PDOException $e) {
+            $this->pdo->rollBack();
+            $this->fail("La base de données n'est pas accessible", $e);
+        }
 
     }
 
